65c80d3bcfdb6b92be1c0769aa6dbd6437a4dc73
leonhardt <jacob.leonhardt649@gmail.com>
2021-05-03 16:00:34 -0500

    post route for sign-in

--- express-project-starter/app.js
+++ express-project-starter/app.js
@@ -39,6 +39,7 @@ store.sync();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
