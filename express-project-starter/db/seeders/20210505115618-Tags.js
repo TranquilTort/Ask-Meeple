19d2530d2fbcc95fdc4965972930f0caf9932f99
Danny <dchen284@gmail.com>
2021-05-05 09:32:01 -0500

    add tag seeder, move new-post routes to index

---
+++ express-project-starter/db/seeders/20210505115618-Tags.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+      return queryInterface.bulkInsert('Tags', [
+        {name: 'Ask for Recommendations for Buying'},
+        {name: 'Rules Clarification'},
+        {name: 'Review'},
+        {name: 'Strategy'}
+        ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+      return queryInterface.bulkDelete('Tags', null, {});
+  }
+};
--- express-project-starter/routes/index.js
+++ express-project-starter/routes/index.js
@@ -1,14 +1,90 @@
 var express = require('express');
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
+const db = require('../db/models');
+const {check,validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs');
+const { Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
+const {requireAuth} = require('../auth.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
     title: 'a/A Express Skeleton Home',
 
-    authenticated: res.locals.authenticated,
+    // authenticated: res.locals.authenticated,
   });
-    // res.send('wahaoo');
+
 });
+
+//Validators
+
+const postValidators = [
+
+  check('title')
+    .exists({checkFalsy:true})
+    .withMessage('Please enter a Title for the post.'),
+  check('body')
+    .exists({checkFalsy:true})
+    .withMessage('Please provide text in the Body for the post.'),
+];
+
+
+router.get('/new-post', csrfProtection, asyncHandler(async (req, res) => {
+
+  const post = db.Post.build();
+
+  // const tags = await db.Tag.findAll({})
+  const tags = [{name: 'Ask for Recommendations for Buying'}, {name:'Rules Clarification'},{name:'Review'},{name:'Strategy'}];
+
+  res.render('post-form', {
+    post,
+    tags,
+    title: 'Post Creation',
+    token: req.csrfToken(),
+  });
+}));
+
+router.post('/new-post', csrfProtection, postValidators, requireAuth, asyncHandler(async (req, res) => {
+  const { title, body, image_url } = req.body
+
+  // const tags = await db.Tag.findAll({})
+  const tags = [{name: 'Ask for Recommendations for Buying'}, {name:'Rules Clarification'},{name:'Review'},{name:'Strategy'}];
+
+  const post = db.Post.build(
+    {
+      body,
+      image_url,
+      title,
+      user_id: req.session.auth.userId,
+    }
+  );
+
+  // console.log(JSON.stringify(post,null,4));
+
+  //need to extract and save tags to database
+  //tags.push(req.body[tag-i]), use for loop to iterate i
+
+  const validatorErrors = validationResult(req);
+  let errors = [];
+
+  if(validatorErrors.isEmpty()) {
+    await post.save();
+    return res.redirect('/');
+  }
+  else {
+    errors = validatorErrors.array().map((error) => error.msg);
+  }
+
+  res.render('post-form', {
+    post,
+    tags,
+    title: 'Post Creation',
+    token: req.csrfToken(),
+    errors,
+  });
+
+}));
 
 module.exports = router;
