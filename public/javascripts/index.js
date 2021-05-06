8a70aeaf38af8e0c85b51364c81ea9068625844e
leonhardt <jacob.leonhardt649@gmail.com>
2021-05-06 17:35:32 -0500

    responsive navigation style

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,19 @@
-window.addEventListener("load", (event)=>{
-    // console.log("hello from javascript!")
-})
+window.addEventListener("DOMContentLoaded", (event)=>{
+    const mobileNav = document.querySelector('.mobile-menu');
+    const mobileMenu = document.querySelector('#mobile-navigation');
+    const outsideNav = document.querySelector('.content-block');
 
+    mobileNav.addEventListener('click', (e) => {
+        mobileMenu.style.display = 'block';
+    });
+
+    outsideNav.addEventListener('click', (e) => {
+        mobileMenu.style.display = 'none';
+    });
+});
+
+
+
+// window.addEventListener("load", (event)=>{
+//     // console.log("hello from javascript!")
+// })
