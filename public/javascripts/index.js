029dfa542263246f205fa165abd472893c7adbe6
Danny <dchen284@gmail.com>
2021-05-07 14:35:52 -0500

    add delete buttons, fix unique idx, fix offset, add search links to tags

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", (event)=>{
     const mobileNav = document.querySelector('.mobile-menu');
     const mobileMenu = document.querySelector('#mobile-navigation');
     const outsideNav = document.querySelector('#content-block');
+    const postArea = document.querySelector('#post-area');
 
     window.addEventListener('resize', (e) => {
         mobileMenu.style.display = 'none';
@@ -18,4 +19,41 @@ window.addEventListener("DOMContentLoaded", (event)=>{
     outsideNav.addEventListener('click', (e) => {
         mobileMenu.style.display = 'none';
     });
+
+    if (postArea) {
+        postArea.addEventListener('click', async (e)=> {
+
+            e.preventDefault();
+
+            if (e.target.id) { //if the target has an id
+
+                const firstPartOfTargetId = e.target.id.slice(0,'delete-post-btn-'.length);
+                if (firstPartOfTargetId === 'delete-post-btn-') { //if the target is a delete button
+
+                    //get the id number of the comment
+                    const postId = parseInt(e.target.id.slice('delete-post-btn-'.length),10);
+
+                    //get the path for fetch
+                    const path = `/posts/${postId}`;
+
+
+                    //fetch to delete comment
+                    const res = await fetch(path, {
+                        method: 'DELETE',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify( {} ),
+                    });
+
+                    const fetchedPost = await res.json();
+
+                    const postToDelete = document.querySelector(`#post-${postId}`);
+                    postToDelete.remove();
+
+                    //repopulate children of comments list
+                    // replaceCommentsHelperFunction(fetchedComments);
+                }
+
+            }
+        });
+    }
 });
