77b0da1b37ec15edcbffd9262e8cd1db26d8e006
leonhardt <jacob.leonhardt649@gmail.com>
2021-05-07 12:25:50 -0500

    seed data and fix home next page content

--- db/seeders/20210504162909-Comments.js
+++ db/seeders/20210504162909-Comments.js
@@ -7,22 +7,36 @@ module.exports = {
 
         {
         user_id: 3,
-        body: "COMMENT 1 ",
-        post_id: 1,
+        body: "Good friends. Like, legit. I've played a few RPG campaigns and really, the game is fun, but the people make the game.",
+        post_id: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        user_id: 2,
-        body: "COMMENT 2",
+        user_id: 8,
+        body: "I agree with MrGreen. It's totally the poeple. Good friends make a bad game fun, and a not great group can drain all the fun out of any game. I'd say just grab some friends and pick a game. Depending on what games your friends like to play, and how long you have, I'd suggest something like Space Team for shorter games, Horrified for medium games, and Scythe for longer games (if you guys can handle it haha).",
+        post_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 6,
+        body: "Don't know if this is considered 'Adventure' or 'Strategy' but I like Everdell. It's a cute game. X)",
         post_id: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         user_id: 2,
-        body: "COMMENT 3",
+        body: "I kinda liked Forgot Waters. But it may also depend on who you/'re palying with. For playing with kids, something like Princess Bride Adventure Book game would be good. But for grown-ups, something like LotR: Journeys in Middle-Earch could work.",
         post_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 7,
+        body: "Had a similar problem. I found that Carcassonne was the most successful game to get my parents into. I tried Ticket to Ride and they didn't get into it all that much. But my mom is obsessed with Carcassonne. Rummikube is also another one they play alot. It's kinda a classic.",
+        post_id: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
--- db/seeders/20210506153951-Post_Tags.js
+++ db/seeders/20210506153951-Post_Tags.js
@@ -2,16 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('Post_Tags',
     [
       {tag_id: 2, post_id: 2, createdAt: new Date(), updatedAt: new Date(),}
@@ -19,13 +10,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
     return queryInterface.bulkDelete('Post_Tags', null, {});
   }
 };
