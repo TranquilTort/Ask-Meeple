cbd2ce2eae7e2156df9b7b806347539d6c11057e
Chris Regan <chrisreg007@gmail.com>
2021-07-27 17:39:16 -0400

    100 users

--- db/seeders/20210405155412-Users.js
+++ db/seeders/20210405155412-Users.js
@@ -1,20 +1,15 @@
 'use strict';
-
+const userCount = 100;
+let insertArray = [{username: 'DemoUser',        email: 'demo@user.com',     hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()}];
+for(let i = 1; i < userCount;i++){
+  insertArray.push({username:`user-${i}`,email:`user-${i}@email.com`, hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()})
+}
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('Users',
-      [
-        {username: 'DemoUser',        email: 'demo@user.com',     hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'ProfPlum',        email: 'purple@clue.com',     hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'MrGreen',         email: 'green@clue.com',    hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'MissScarlet',     email: 'red@clue.com',      hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'PrimPeacock',     email: 'blue@clue.com',      hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'MaidWhite',       email: 'white@clue.com',      hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'ColMustard',      email: 'yellow@clue.com',      hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'DrOrchid',        email: 'orchid@clue.com',      hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-        {username: 'MrBoddy',         email: 'boddy@clue.com',      hashedPassword: '$2a$10$DwtHNVxY1gx9nsanxl2rRecoraJOKpLlUfOnwb1UlWQi1spDfY3Jy', createdAt: new Date(), updatedAt: new Date()},
-      ], {});
+      insertArray
+      , {});
   },
 
   down: (queryInterface, Sequelize) => {
